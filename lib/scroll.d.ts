35f395dca040876958035145316430171e46ece3
Joseph Peduto <joe@MacBook-Pro.local>
2020-12-10 10:23:51 -0500

    Bugfix

--- lib/scroll.d.ts
+++
@@ -1,3 +0,0 @@
-export declare function turbolinksScrollSetup(document: Document): void;
-export declare function turbolinksPersistScrollForNextVisit(): void;
-export declare function turbolinksScrollsetScrollPosition(): void;
